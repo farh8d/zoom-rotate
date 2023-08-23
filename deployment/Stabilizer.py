ee70163c010890e66b25269704a2c81418aa825f
farh8d <farhad.h@pirelly.com>
2023-08-23 10:15:32 +0330

    add yolo car detection before angle estimation.

--- deployment/Stabilizer.py
+++ deployment/Stabilizer.py
@@ -1,73 +1,33 @@
 import numpy as np
 import cv2
-import os
 from ultralytics import YOLO
-from google.cloud import storage
+from torchvision import transforms
+import onnxruntime
+import numpy as np 
+from PIL import Image
 
 
 
-
-class ZoomRotate:
-    def __init__(self):
-        # model_path = '/tmp/yolov8s_6_2023.pt'                           # for google cloud
-        model_path = 'localpackages/model/yolov8s_6_2023.pt'              # for local run
+class Stabilizer:
+    def __init__(self , car_detection_model_path , angle_estimation_model_path):
+        
         self.bb = {}
 
-
-        if not os.path.exists(model_path):
-            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(os.environ.get('MY_CODE_DIR', '/workspace'),
-                                                                        'localpackages/credentials/pirelly360-ai-deployment-firebase-adminsdk-txp88-8d93127582.json')
-            self.download_file_from_gcs()
-
+        try:
+            self.model_yolo =  YOLO(car_detection_model_path) 
+        except Exception as e :
+            print("constructor Exception - yolo",e)
 
         try:
-            self.model_yolo =  YOLO(model_path) 
+            self.angle_input_size = 420 
+            self.angle_model = onnxruntime.InferenceSession(angle_estimation_model_path)
+            self.angle_transforms =  transforms.Compose([
+                transforms.Resize((self.angle_input_size , self.angle_input_size)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
         except Exception as e :
-            print("constructor Exception",e)
-
-
-
-    
-
-
-    def download_file_from_gcs(self, bucket_name="pirelly360-ai-deployment.appspot.com",
-                               file_name="yolov8s_6_2023.pt", local_directory="/tmp/",
-                               new_file_name=None):
-        """
-        Downloads a file from Google Cloud Storage and saves it to a local directory,
-        but only if the file does not already exist in the local directory.
-
-        Parameters:
-        bucket_name (str): the name of the Google Cloud Storage bucket.
-        file_name (str): the path of the file in the Google Cloud Storage bucket.
-        local_directory (str): the local directory where the file should be saved.
-        new_file_name (str, optional): the new name for the downloaded file. If not specified,
-            the original file name will be used.
-
-        Returns:
-        None
-        """
-        # Initialize a client object
-        client = storage.Client()
-
-        # Get the bucket and file objects
-        bucket = client.get_bucket(bucket_name)
-        blob = bucket.blob(file_name)
-
-        # Set the destination file name
-        if new_file_name is None:
-            new_file_name = os.path.basename(file_name)
-        destination_file_name = os.path.join(local_directory, new_file_name)
-
-        # Download the file only if it doesn't already exist in the local directory
-        if not os.path.exists(destination_file_name):
-            # Create the local directory if it doesn't exist
-            if not os.path.exists(local_directory):
-                os.makedirs(local_directory)
-
-            # Download the file to the local directory with the new name
-            blob.download_to_filename(destination_file_name)
-
+            raise Exception("constructor Exception - yolo ",e)
 
 
 
@@ -115,11 +75,10 @@ class ZoomRotate:
         try:
             self.bb = self.__detect_main_car(img)  
         except Exception as e :
-            print("detect_main_car Exception",e)
+            raise Exception("centerizer -> detect_main_car Exception",e)
 
         if self.bb is None:
-            print("no car!!!")
-            return img
+            raise Exception("centerizer -> detect_main_car No detection")
 
         shift_x = (img.shape[1] / 2) -  int((self.bb['xmax'] + self.bb['xmin']) / 2)
         shift_y = (img.shape[0] / 2) - int((self.bb['ymax'] + self.bb['ymin']) / 2)
@@ -160,12 +119,11 @@ class ZoomRotate:
 
 
 
-    def  zoomIN_zoomOut(self , img , height_fraction = 0.45  ):
+    def  zoomIN_zoomOut(self , img , height_fraction):
         # get cv2 image
         # ckeck any car detection
         if self.bb is None:
-            print("no car!!!")
-            return img
+            raise Exception("zoomIN_zoomOut -> detect_main_car No detection")
         h = self.bb['ymax'] - self.bb['ymin']
         if (height_fraction - 0.01) * img.shape[0] < h < (height_fraction + 0.01) * img.shape[0]:
             return img
@@ -173,7 +131,7 @@ class ZoomRotate:
         try:
             final = self.__make_new_image(img , int(height_fraction * img.shape[0]) ) 
         except Exception as e :
-            print("make_new_image Exception",e)
+            raise Exception("make_new_image Exception",e)
          
         return final
     
@@ -187,6 +145,40 @@ class ZoomRotate:
         rotation_matrix = cv2.getRotationMatrix2D((width/2, height/2), angle, 1)
         rotated_image = cv2.warpAffine(img, rotation_matrix, (width, height))
         return rotated_image
+    
+
+
+    def estimate_angle(self , img):
+        try:
+            bb = self.__detect_main_car(img)  
+        except Exception as e :
+            raise Exception("estimate angle -> detect_main_car Exception",e)
+        if bb is None:
+            raise Exception("estimate angle -> detect_main_car No detection")
+
+        img1 = img[int(bb["ymin"]):int(bb["ymax"]) , int(bb["xmin"]):int(bb["xmax"])]
+        img1 = Image.fromarray(img1)
+        img1 = self.angle_transforms(img1)
+        img1 = np.array(img1)
+        img1 = np.expand_dims(img1, 0).astype(np.float32)
+        return self.angle_model.run(None, {"input":img1})[0][0][0]
+         
+
+    def run(self , img_address , output_address , height_fraction = 0.35):  # Done: return True  Failed: throw exception
+        try:
+            img = cv2.imread(img_address)
+            angle = self.estimate_angle(img) 
+            img1 = self.rotate_image(img , -angle)
+            img1 = self.centerizer(img1)
+            img1 = self.zoomIN_zoomOut(img1 , height_fraction) 
+            cv2.imwrite(output_address , img1) 
+
+        except Exception as e :
+            raise Exception("stab run method Exception",e)
+        
+        return True
+        
+
 
         
 
